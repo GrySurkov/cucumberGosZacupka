f633420b5cee48bf40cb186b3583ce39b7579ea7
GriSurkov <greg2004@bk.ru>
2019-02-08 16:22:29 +0300

    v0.0.2

--- src/main/java/stepdefs/Test2.java
+++ src/main/java/stepdefs/Test2.java
@@ -21,6 +21,7 @@ public class Test2 {
     public void i_am_on_page(String url) {
         System.setProperty("webdriver.chrome.driver", "D:\\JavaProject\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(url);
     }
@@ -37,7 +38,7 @@ public class Test2 {
 
     @Then("^i found element by id \"([^\"]*)\" and enter \"([^\"]*)\"$")
     public void i_found_element_by_id_and_entering(String id, String data) {
-         driver.findElement(By.id(id)).sendKeys(data);
+        driver.findElement(By.id(id)).sendKeys(data);
     }
 
     @Then("^i found element by cssSelector \"([^\"]*)\" and enter \"([^\"]*)\"$")
@@ -50,17 +51,39 @@ public class Test2 {
         driver.findElement(By.id(id)).click();
     }
 
+    @Then("^i found element by cssSelector \"([^\"]*)\" and click them$")
+    public void i_found_element_by_cssSelector_and_click_them(String cssSelector) {
+        driver.findElement(By.cssSelector(cssSelector)).click();
+    }
+
     @Then("^i get list of elements by class name\"([^\"]*)\" and choose element \"([^\"]*)\"$")
     public void i_get_list_of_elements_by_class_name_and_choose_element(String className, String element) {
         WebElement ul = driver.findElement(By.className(className));
         List<WebElement> li = ul.findElements(By.xpath(".//li"));
-        for(int i=0; i<li.size(); i++){
+        for (int i = 0; i < li.size(); i++) {
             String str = li.get(i).getText();
-                if(str.toLowerCase().contains(element.toLowerCase())){
+            if (str.toLowerCase().contains(element.toLowerCase())) {
                 li.get(i).findElement(By.xpath("./span")).click();
                 return;
             }
         }
+    }
 
+    @Then("^i get list of elements by cssSelector \"([^\"]*)\" and choose elements \"([^\"]*)\"$")
+    public void i_get_list_of_elements_by_cssSelector_and_choose_elements(String cssSelector, String element) {
+        WebElement ul = driver.findElement(By.cssSelector(cssSelector));
+        List<WebElement> li = ul.findElements(By.xpath(".//li"));
+        for (int i = 0; i < li.size(); i++) {
+            String str = li.get(i).getText();
+            if (str.toLowerCase().contains(element.toLowerCase())) {
+                if (!(li.get(i).findElement(By.xpath("./input")).isSelected())) {
+                    li.get(i).findElement(By.xpath("./label")).click();
+                }
+            } else {
+                if (li.get(i).findElement(By.xpath("./input")).isSelected()) {
+                    li.get(i).findElement(By.xpath("./label")).click();
+                }
+            }
+        }
     }
 }
--- target/classes/second.feature
+++ target/classes/second.feature
@@ -1,6 +1,6 @@
 @goszacupka
 Feature: second
-    Scenario: S2
+    Scenario: Enter data on form
         When i am on "http://www.zakupki.gov.ru" page
         Then i see "Закупки" and click them
         Then i see "Расширенный поиск" and click them
@@ -9,3 +9,11 @@ Feature: second
         Then i found element by id "priceToGeneral" and enter "3000000"
         Then i found element by id "currencyChangecurrencyIdGeneral" and click them
         Then i get list of elements by class name"tuningQuickSearchList" and choose element "Российский рубль"
+        Then i found element by cssSelector "#placingWaysTag > div > div.collapsed.height30" and click them
+        Then i get list of elements by cssSelector "#placingWaysTag > div > div.selectChose > div.expanded" and choose elements "Аукцион"
+        Then i found element by id "placingWaysSelectBtn" and click them
+        Then i found element by cssSelector "#orderStages > div > div.collapsed.height30" and click them
+        Then i get list of elements by cssSelector "#orderStages > div > div.selectChose > div.expanded > ul" and choose elements "подача заявок"
+        Then i found element by cssSelector "#orderStagesSelectBtn > span" and click them
+        Then i found element by cssSelector "#quickSearchForm_header > div.parametrs.margBtm10 > div > div.darkGreyBox.saveExtendedSearch > div.floatRight" and click them
+
